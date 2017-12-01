1323545c70293a7fe5f2d2cac268170393c11006
dgrubb <davidanthonygrubb@gmail.com>
2017-12-01 12:40:01 -0600

    Add a button mapping type to identify what address/data pins belong to which controller buttons.

--- Firmware/jaguar/jaguar-controller.c
+++ Firmware/jaguar/jaguar-controller.c
@@ -8,3 +8,31 @@
 
 #include "jaguar-controller.h"
 
+
+const char* jaguar_get_button_str(jaguar_button_t button)
+{
+    switch (button) {
+        case JAGUAR_BUTTON_NORTH:  return "North";
+        case JAGUAR_BUTTON_SOUTH:  return "South";
+        case JAGUAR_BUTTON_EAST:   return "East";
+        case JAGUAR_BUTTON_WEST:   return "West";
+        case JAGUAR_BUTTON_A:      return "A";
+        case JAGUAR_BUTTON_B:      return "B";
+        case JAGUAR_BUTTON_C:      return "C";
+        case JAGUAR_BUTTON_OPTION: return "Option";
+        case JAGUAR_BUTTON_START:  return "Start";
+        case JAGUAR_BUTTON_ZERO:   return "Zero";
+        case JAGUAR_BUTTON_ONE:    return "One";
+        case JAGUAR_BUTTON_TWO:    return "Two";
+        case JAGUAR_BUTTON_THREE:  return "Three";
+        case JAGUAR_BUTTON_FOUR:   return "Four";
+        case JAGUAR_BUTTON_FIVE:   return "Five";
+        case JAGUAR_BUTTON_SIX:    return "Six";
+        case JAGUAR_BUTTON_SEVEN:  return "Seven";
+        case JAGUAR_BUTTON_EIGHT:  return "Eight";
+        case JAGUAR_BUTTON_NINE:   return "Nine";
+        case JAGUAR_BUTTON_STAR:   return "Star";
+        case JAGUAR_BUTTON_POUND:  return "Pound";
+        default:                   return "Unknown";
+    }
+}
--- Firmware/jaguar/jaguar-controller.h
+++ Firmware/jaguar/jaguar-controller.h
@@ -10,10 +10,41 @@
 #define _JAGUAR_CONTROLLER_H
 
 #include <stdint.h>
+#include "stm32f0xx_hal_gpio.h"
+
+typedef enum {
+    JAGUAR_BUTTON_NORTH = 0,
+    JAGUAR_BUTTON_SOUTH,
+    JAGUAR_BUTTON_EAST,
+    JAGUAR_BUTTON_WEST,
+    JAGUAR_BUTTON_A,
+    JAGUAR_BUTTON_B,
+    JAGUAR_BUTTON_C,
+    JAGUAR_BUTTON_OPTION,
+    JAGUAR_BUTTON_START,
+    JAGUAR_BUTTON_ZERO,
+    JAGUAR_BUTTON_ONE,
+    JAGUAR_BUTTON_TWO,
+    JAGUAR_BUTTON_THREE,
+    JAGUAR_BUTTON_FOUR,
+    JAGUAR_BUTTON_FIVE,
+    JAGUAR_BUTTON_SIX,
+    JAGUAR_BUTTON_SEVEN,
+    JAGUAR_BUTTON_EIGHT,
+    JAGUAR_BUTTON_NINE,
+    JAGUAR_BUTTON_STAR,
+    JAGUAR_BUTTON_POUND
+} jaguar_button_t;
+
+typedef struct {
+    uint16_t address_pin;
+    uint16_t data_pin;
+    GPIO_PinState state;
+} jaguar_button_state_t;
 
 void jaguar_update_inputs();
 void jaguar_get_pending_transition();
-
+const char* jaguar_get_button_str(jaguar_button_t button);
 
 #endif /* _JAGUAR_CONTROLLER_H */
 
