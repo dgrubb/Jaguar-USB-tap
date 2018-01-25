ae0eb1a47f997604ce07faefcd8165b54ae51eec
David Grubb <davidanthonygrubb@gmail.com>
2018-01-25 12:54:20 -0600

    Add a shiny new VID/PID, courtesy of pid.codes

--- Firmware/Src/usbd_desc.c
+++ Firmware/Src/usbd_desc.c
@@ -71,10 +71,11 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */ 
-#define USBD_VID     1155
+#define USBD_VID     1209
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "dgrubb"
-#define USBD_PID_FS     22315
+//#define USBD_PID_FS     22315
+#define USBD_PID_FS     1986
 #define USBD_PRODUCT_STRING_FS     "Jaguar USB Tap"
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "HID Config"
